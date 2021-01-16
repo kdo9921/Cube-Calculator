3805ff8174dad4a433fea0dca9d026720cc1cb07
키위 <kdo9921@apteryx.moe>
2021-01-17 03:01:27 +0900

    cube calculator

---
+++ cube.js
@@ -0,0 +1,40 @@
+var cube = {
+    classUp : [[6,15,4.8],[2,3.5,2],[0.6,1.4,0.5]],   //[goal][red black addi]
+    img : ["red","black","addi"],
+    color : ["red","black","#7fff00"],
+    cubeName : ["레드","블랙","에디"],
+    currentCube : 1,
+    goalPotential : 2
+};
+
+function getRadioIndex(nameValue) { //name is cube or potential (string)
+    for (var i=0; i<document.getElementsByName(nameValue).length; i++) {
+        if (document.getElementsByName(nameValue)[i].checked) {
+            return i;
+        }
+    }
+}
+function changeCube() {
+    cube.currentCube = getRadioIndex("cube");
+    document.getElementById("cube_kind").innerHTML = cube.cubeName[cube.currentCube] + " 큐브";
+    document.getElementById("cube_img").src = "./img/" + cube.img[cube.currentCube] + ".png";
+    document.getElementsByClassName('info_item')[0].style.borderColor = cube.color[cube.currentCube];
+    for (var i=0;i<3;i++) {
+        document.getElementsByClassName('cubePercent')[i].value = cube.classUp[i][cube.currentCube];
+    }
+}
+
+function changeGoal() {
+    cube.goalPotential = getRadioIndex("potential");
+}
+
+function calculation() {
+    var customClassUp = document.getElementsByClassName('cubePercent')[cube.goalPotential].value;
+    if (document.getElementById('miracle').checked) {
+        customClassUp = customClassUp * 2;
+    }
+    var fail = (100 - customClassUp) / 100;
+    var amount = Number(document.getElementById('amount').value);
+    var failPer = fail ** amount;
+    document.getElementById('succesUp').innerHTML = ((1 - failPer)*100).toFixed(3);
+}
